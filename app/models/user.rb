ed033b7f0cf955dac70858b5010a9b589b688df8
Fadhil bin Luqman <fadhil.luqman@gmail.com>
2013-08-18 00:32:27 +0800

    remove trackable;

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable :validatable#,trackable
   has_one :friendship, :dependent => :destroy
 
   # Setup accessible (or protected) attributes for your model
